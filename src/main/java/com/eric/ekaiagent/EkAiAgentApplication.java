f4e698aa90c78f28d44fa1170449fa4089e74797
wangp126 <ericwang@email.com>
2025-06-21 21:29:22 -0400

    add inits

---
+++ src/main/java/com/eric/ekaiagent/EkAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.eric.ekaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkAiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkAiAgentApplication.class, args);
+	}
+
+}
