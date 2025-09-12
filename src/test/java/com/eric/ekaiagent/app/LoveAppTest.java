12657bec4dcf929854ce4edfa89dbfffd1882d28
Eric <ericwang7717@gmail.com>
2025-09-11 21:50:38 -0400

    feature(mcp servers):  make a new mcp servers that can search image

--- src/test/java/com/eric/ekaiagent/app/LoveAppTest.java
+++ src/test/java/com/eric/ekaiagent/app/LoveAppTest.java
@@ -71,7 +71,7 @@ class LoveAppTest {
 //        String answer =  loveApp.doChatWithMcp(message, chatId);
 //        Assertions.assertNotNull(answer);
         // 测试图片搜索 MCP
-        String message = "帮我搜索一些哄另一半开心的图片";
+        String message = "help me find a picture of a cute cat";
         String answer =  loveApp.doChatWithMcp(message, chatId);
         Assertions.assertNotNull(answer);
     }
