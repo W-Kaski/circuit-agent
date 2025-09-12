83e9145b5b9944f74e85571f028ad6dc1d94c6ac
Eric <ericwang7717@gmail.com>
2025-09-11 21:10:38 -0400

    feature(mcp servers):   add call ai with mcp servers
    test(mcp servers):
      try tp call ai with mcp

--- src/test/java/com/eric/ekaiagent/app/LoveAppTest.java
+++ src/test/java/com/eric/ekaiagent/app/LoveAppTest.java
@@ -62,4 +62,17 @@ class LoveAppTest {
         testMessage("直接下载一张适合做手机壁纸的星空情侣图片为文件");
 
     }
+
+    @Test
+    void doChatWithMcp() {
+        String chatId = UUID.randomUUID().toString();
+        // 测试地图 MCP
+//        String message = "我的另一半居住在上海静安区，请帮我找到 5 公里内合适的约会地点";
+//        String answer =  loveApp.doChatWithMcp(message, chatId);
+//        Assertions.assertNotNull(answer);
+        // 测试图片搜索 MCP
+        String message = "帮我搜索一些哄另一半开心的图片";
+        String answer =  loveApp.doChatWithMcp(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
 }
