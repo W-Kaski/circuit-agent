f5bbc57dc5db46e82f7f30095c4022ea57846f6b
Eric <ericwang7717@gmail.com>
2025-09-14 23:53:47 -0400

    update(mcp server):  add config for mcp server

---
+++ ek-image-search-mcp-server/src/test/java/com/eric/ekimagesearchmcpserver/EkImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.ekimagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkImageSearchMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
